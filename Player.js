47713f3f5e90e8c480909b633e7862ec4941ad0f
Jorge <jorge.sanchez1376@gmail.com>
2023-08-19 17:34:55 +0200

    Finalizacion de proyecto

--- Player.js
+++ Player.js
@@ -1,9 +1,11 @@
 export default class Player{
     rightPressed = false;
     leftPressed = false;
-    constructor(canvas,velocity){
+    shootPressed = false;
+    constructor(canvas,velocity,bulletController){
         this.canvas = canvas;
         this.velocity = velocity;
+        this.bulletController = bulletController;
         this.x = this.canvas.width / 2;
         this.y = this.canvas.height -75;
         this.width = 50;
@@ -15,7 +17,30 @@ export default class Player{
         document.addEventListener("keyup",this.keyup);
     }
     draw(ctx){
+        if(this.shootPressed){
+            this.bulletController.shoot(this.x+this.width/2,this.y,4,10);
+        }
+        this.move();
+        this.collideWithWalls();
         ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
+    }
+    collideWithWalls(){
+        //left
+        if(this.x <0){
+            this.x=0;
+        }
+        //right
+        if(this.x > this.canvas.width -this.width){
+            this.x = this.canvas.width -this.width;
+            //this.width (Player width)
+        }
+    }
+    move(){
+        if(this.rightPressed){
+            this.x += this.velocity; 
+        }else if(this.leftPressed){
+            this.x += -this.velocity;
+        }
     }
    keydown = event => {
         if(event.code == 'ArrowRight'){
@@ -24,6 +49,9 @@ export default class Player{
         if(event.code == 'ArrowLeft'){
             this.leftPressed = true;
         }
+        if(event.code == "Space"){
+            this.shootPressed = true;
+        }
    }
    keyup = event => {
     if(event.code == 'ArrowRight'){
@@ -31,6 +59,9 @@ export default class Player{
     }
     if(event.code == 'ArrowLeft'){
         this.leftPressed = false;
+    }
+    if(event.code == "Space"){
+        this.shootPressed = false;
     }
 }
 }
